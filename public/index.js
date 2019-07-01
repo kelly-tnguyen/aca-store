62371c6989e990761e632231b3fdde97f0f367ba
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-07-01 13:45:40 -0500

    in progress

--- public/index.js
+++ public/index.js
@@ -1,21 +1,28 @@
-// have search bar with enter button
-// show name, rating, number of reviews and price
-// include a link or button that should be clicked on so that you can see the details of the product
-
 function Products(products){
         let productDivs = "";
 
         for (let i=0; i < products.length; i++) {
             let product = products[i];
-            productDivs +=`<div>${product.name}</div>`
+            productDivs += 
+            `<div id = 'product'>
+                <div><h2 id ='${product.id}'>${product.name}</h2></div>
+                <div><li>Description: ${product.description}</li></div>
+                <div><li>${product.price}</li></div>
+                <div><li>Category: ${product.category}</li></div>
+                <div><li>Rating (5 is the highest): ${product.rating}</li></div>
+             </div>`
+        
         }
     document.getElementById("products").innerHTML = productDivs;
 }
 Products(products);
 
-function search(){
-    let searchWord = document.getElementById("search").value;
 
-    let filteredProducts = products.filter(p=>p.name === searchWord)
+function search(){
+    let searchWord = document.getElementById("searchBox").value;
+    //Shows exact mathing 
+    let filteredProducts = products.filter(p => p.name === searchWord)
+    
     Products(filteredProducts);
 }
+
